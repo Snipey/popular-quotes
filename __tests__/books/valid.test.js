2fee6f79d2d4a68ee024246da0cb83f223f74c16
Aditya <aditya005@gmail.com>
2019-10-06 23:24:03 -0700

    updates * updating package scripts * updating snapshots * fixing tests

--- __tests__/books/valid.test.js
+++ __tests__/books/valid.test.js
@@ -2,32 +2,32 @@ const data = require('../../data/books.json');
 
 describe('is the Book quote valid', () => {
 	test('All items need to have a year', () => {
-		data.forEach((quotes) => {
-			expect(quotes).toMatchSnapshot({
+		data.forEach((years) => {
+			expect(years).toMatchSnapshot({
 			  year: expect.any(Number)
 			});
 		});
 	});
 	
 	test('All items need to have a title', () => {
-		data.forEach((quotes) => {
-			expect(quotes).toMatchSnapshot({
+		data.forEach((titles) => {
+			expect(titles).toMatchSnapshot({
 			  title: expect.any(String)
 			});
 		});
 	});
 	
 	test('All items need to have a author', () => {
-		data.forEach((quotes) => {
-			expect(quotes).toMatchSnapshot({
+		data.forEach((authors) => {
+			expect(authors).toMatchSnapshot({
 			  author: expect.any(String)
 			});
 		});
 	});
 
 	test('All items need to have a character', () => {
-		data.forEach((quotes) => {
-			expect(quotes).toMatchSnapshot({
+		data.forEach((characters) => {
+			expect(characters).toMatchSnapshot({
 			  character: expect.any(String)
 			});
 		});
