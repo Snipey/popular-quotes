1fb934760ac7ff6c65cd6dea56db2028f68a07cb
Restyled.io <commits@restyled.io>
2019-10-07 17:29:41 +0000

    Restyled by prettier

--- __tests__/books/valid.test.js
+++ __tests__/books/valid.test.js
@@ -1,33 +1,33 @@
-const data = require('../../data/books.json');
+const data = require("../../data/books.json");
 
-describe('is the Book quote valid', () => {
-  test('All items need to have a year', () => {
-    data.forEach((quotes) => {
-      expect(quotes).toMatchSnapshot({year : expect.any(Number)});
+describe("is the Book quote valid", () => {
+  test("All items need to have a year", () => {
+    data.forEach(quotes => {
+      expect(quotes).toMatchSnapshot({ year: expect.any(Number) });
     });
   });
 
-  test('All items need to have a title', () => {
-    data.forEach((quotes) => {
-      expect(quotes).toMatchSnapshot({title : expect.any(String)});
+  test("All items need to have a title", () => {
+    data.forEach(quotes => {
+      expect(quotes).toMatchSnapshot({ title: expect.any(String) });
     });
   });
 
-  test('All items need to have a author', () => {
-    data.forEach((quotes) => {
-      expect(quotes).toMatchSnapshot({author : expect.any(String)});
+  test("All items need to have a author", () => {
+    data.forEach(quotes => {
+      expect(quotes).toMatchSnapshot({ author: expect.any(String) });
     });
   });
 
-  test('All items need to have a character', () => {
-    data.forEach((quotes) => {
-      expect(quotes).toMatchSnapshot({character : expect.any(String)});
+  test("All items need to have a character", () => {
+    data.forEach(quotes => {
+      expect(quotes).toMatchSnapshot({ character: expect.any(String) });
     });
   });
 
-  test('All items need to have a quote', () => {
-    data.forEach((quotes) => {
-      expect(quotes).toMatchSnapshot({quote : expect.any(String)});
+  test("All items need to have a quote", () => {
+    data.forEach(quotes => {
+      expect(quotes).toMatchSnapshot({ quote: expect.any(String) });
     });
   });
 });
