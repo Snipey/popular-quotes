f9ab64d1c130a25bb5d772ed5eef653c530e50e4
Aditya <aditya005@gmail.com>
2019-10-07 00:56:14 -0700

    updating test content for books related tests

--- __tests__/books/valid.test.js
+++ __tests__/books/valid.test.js
@@ -2,36 +2,37 @@ const data = require('../../data/books.json');
 
 describe('is the Book quote valid', () => {
 	test('All items need to have a year', () => {
-		data.forEach((years) => {
-			expect(years).toMatchSnapshot({
+		data.forEach((quotes) => {
+			expect(quotes).toMatchSnapshot({
 			  year: expect.any(Number)
 			});
 		});
 	});
 	
 	test('All items need to have a title', () => {
-		data.forEach((titles) => {
-			expect(titles).toMatchSnapshot({
+		data.forEach((quotes) => {
+			expect(quotes).toMatchSnapshot({
 			  title: expect.any(String)
 			});
 		});
 	});
 	
 	test('All items need to have a author', () => {
-		data.forEach((authors) => {
-			expect(authors).toMatchSnapshot({
+		data.forEach((quotes) => {
+			expect(quotes).toMatchSnapshot({
 			  author: expect.any(String)
 			});
 		});
 	});
 
 	test('All items need to have a character', () => {
-		data.forEach((characters) => {
-			expect(characters).toMatchSnapshot({
+		data.forEach((quotes) => {
+			expect(quotes).toMatchSnapshot({
 			  character: expect.any(String)
 			});
 		});
 	});
+
 	test('All items need to have a quote', () => {
 		data.forEach((quotes) => {
 			expect(quotes).toMatchSnapshot({
